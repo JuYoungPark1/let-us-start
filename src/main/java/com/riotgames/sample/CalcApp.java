9c6c28b4f2c2c8d44bd6702d6d7eabe2b3b37a34
JuYoung Park <qkrwndud1994@naver.com>
2017-04-07 00:58:17 +0900

    changed calcApp

--- src/main/java/com/riotgames/sample/CalcApp.java
+++ src/main/java/com/riotgames/sample/CalcApp.java
@@ -1,25 +1,89 @@
 package com.riotgames.sample;
 
-import java.util.Arrays;
+import java.util.*;
+
 
 /**
  * Calculator application
  */
 public class CalcApp {
-    public double calc(String[] tokens) {
-        final double firstOperand;
-        final double secondOperand;
-        firstOperand = Double.parseDouble(tokens[0]);
-        if (tokens.length > 2) {
-            secondOperand = Double.parseDouble(tokens[2]);
-        } else {
-            secondOperand = Double.parseDouble(tokens[1]);
-        }
-        final Operator operator = Operator.findOperator(tokens[1]);
-
-        return operator.evaluate(firstOperand, secondOperand);
+	public double calc(String[] tokens) {
+    	Queue<String> s = new LinkedList();
+    	for(int i = 0 ; i < tokens.length ; i++){
+    		s.add(tokens[i]);
+    	}
+    	
+    	return gualho(s);
 
     }
+    
+    public double gualho(Queue<String> s){
+    	
+    	Queue<String> tempq = new LinkedList();
+    	
+    	for(;!(s.isEmpty());){
+    		
+    		String polls = s.poll();
+    		
+    		if(polls.equals("(")){
+    			tempq.add(""+ gualho(s) +"");
+    		}
+    		else if(polls.equals(")")){
+    			return doudiv(tempq);
+    		}
+    		else{
+    			tempq.add(polls);
+    		}
+    	}
+    	
+    	return doudiv(tempq);
+    }
+    
+    public double doudiv(Queue<String> s){
+    	
+    	Queue<String> tempq = new LinkedList();
+    	
+    	for(;!(s.isEmpty());){
+    		
+    		String polls = s.poll();
+    		
+    		if(polls.equals("*")){
+    			double firstOp;
+    			double secondOp;
+    			
+    			firstOp = Double.parseDouble(tempq.poll());
+    			secondOp = Double.parseDouble(s.poll());
+    			Operator Op = Operator.findOperator(polls);
+    			
+    			tempq.add(""+Op.evaluate(firstOp,secondOp)+"");
+    		}
+    		else if(polls.equals("/")){
+    			double firstOp;
+    			double secondOp;
+    			
+    			firstOp = Double.parseDouble(tempq.poll());
+    			secondOp = Double.parseDouble(s.poll());
+    			Operator Op = Operator.findOperator(polls);
+    			
+    			tempq.add(""+Op.evaluate(firstOp,secondOp)+"");
+    		}
+    		else{
+    			tempq.add(polls);
+    		}
+    	}
+    	for(;!(tempq.size()==1);){
+    		double firstOp;
+			double secondOp;
+			
+			secondOp = Double.parseDouble(tempq.poll());
+			Operator Op = Operator.findOperator(tempq.poll());
+			firstOp = Double.parseDouble(tempq.poll());
+			
+			tempq.add(""+Op.evaluate(secondOp,firstOp)+"");
+    	}
+    	return Double.parseDouble(tempq.poll());
+    }
+
 
     public static void main( String[] args ) {
         final CalcApp app = new CalcApp();
