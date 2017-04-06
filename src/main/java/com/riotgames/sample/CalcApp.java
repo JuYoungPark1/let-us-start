d908d6a8a7c20d6b6925e869439496fe6f281d32
JuYoung Park <qkrwndud1994@naver.com>
2017-04-07 02:41:45 +0900

    hi

--- src/main/java/com/riotgames/sample/CalcApp.java
+++ src/main/java/com/riotgames/sample/CalcApp.java
@@ -17,72 +17,79 @@ public class CalcApp {
 
     }
     
-	 public double gualho(Queue<String> s){
-	    	
-	    	Queue<String> tempq = new LinkedList();
-	    	
-	    	for(;!(s.isEmpty());){
-	    		
-	    		String polls = s.poll();
-	    		
-	    		if(")".equals(polls)){
-	    			tempq.add(Double.toString(gualho(s)));
-	    		}
-	    		else if("(".equals(polls)){
-	    			return doudiv(tempq);
-	    		}
-	    		else{
-	    			tempq.add(polls);
-	    		}
-	    	}
-	    	
-	    	return doudiv(tempq);
-	    }
-	    
-	    public double doudiv(Queue<String> s){
-	    	
-	    	Queue<String> tempq = new LinkedList();
-	    	
-	    	for(;!(s.isEmpty());){
-	    		
-	    		String polls = s.poll();
-	    		
-	    		if("*".equals(polls)){
-	    			double firstOp;
-	    			double secondOp;
-	    			
-	    			firstOp = Double.parseDouble(tempq.poll());
-	    			secondOp = Double.parseDouble(s.poll());
-	    			Operator Op = Operator.findOperator(polls);
-	    			
-	    			tempq.add(Double.toString(Op.evaluate(firstOp,secondOp)));
-	    		}
-	    		else if("/".equals(polls)){
-	    			double firstOp;
-	    			double secondOp;
-	    			
-	    			firstOp = Double.parseDouble(tempq.poll());
-	    			secondOp = Double.parseDouble(s.poll());
-	    			Operator Op = Operator.findOperator(polls);
-	    			
-	    			tempq.add(Double.toString(Op.evaluate(firstOp,secondOp)));
-	    		}
-	    		else{
-	    			tempq.add(polls);
-	    		}
-	    	}
-	    	for(;!(tempq.size()==1);){
-	    		double firstOp;
-				double secondOp;
-				
-				secondOp = Double.parseDouble(tempq.poll());
-				Operator Op = Operator.findOperator(tempq.poll());
-				firstOp = Double.parseDouble(tempq.poll());
-				
-				tempq.add(Double.toString(Op.evaluate(secondOp,firstOp)));
-	    	}
-	    	return Double.parseDouble(tempq.poll());
-	    }
+    public double gualho(Queue<String> s){
+    	
+    	Queue<String> tempq = new LinkedList();
+    	
+    	for(;!(s.isEmpty());){
+    		
+    		String polls = s.poll();
+    		
+    		if("(".equals(polls)){
+    			tempq.add(Double.toString(gualho(s)));
+    		}
+    		else if(")".equals(polls)){
+    			
+    			return doudiv(tempq);
+    		}
+    		else{
+    			if("-".equals(polls)){
+    				tempq.add("+");
+    				tempq.add(Double.toString((-Double.parseDouble(s.poll()))));
+    			}
+    			else{
+    				tempq.add(polls);
+    			}
+    		}
+    	}
+    	
+    	return doudiv(tempq);
+    }
+    
+    public double doudiv(Queue<String> s){
+    	
+    	Stack<String> tempq = new Stack();
+    	
+    	for(;!(s.isEmpty());){
+    		
+    		String polls = s.poll();
+    		
+    		if("x".equals(polls)){
+    			double firstOp;
+    			double secondOp;
+    			
+    			firstOp = Double.parseDouble(tempq.pop());
+    			secondOp = Double.parseDouble(s.poll());
+    			Operator Op = Operator.findOperator(polls);
+    			
+    			tempq.add(Double.toString(Op.evaluate(firstOp,secondOp)));
+    		}
+    		else if("/".equals(polls)){
+    			double firstOp;
+    			double secondOp;
+    			
+    			firstOp = Double.parseDouble(tempq.pop());
+    			secondOp = Double.parseDouble(s.poll());
+    			Operator Op = Operator.findOperator(polls);
+    			
+    			tempq.add(Double.toString(Op.evaluate(firstOp,secondOp)));
+    		}
+    		else{
+    			tempq.add(polls);
+    		}
+    	}
+    	for(;!(tempq.size()==1);){
+    		double firstOp;
+			double secondOp;
+			
+			secondOp = Double.parseDouble(tempq.pop());
+			Operator Op = Operator.findOperator(tempq.pop());
+			firstOp = Double.parseDouble(tempq.pop());
+			
+			tempq.add(Double.toString(Op.evaluate(firstOp,secondOp)));
+    	}
+    	return Double.parseDouble(tempq.pop());
+    }
 
 
     public static void main( String[] args ) {
